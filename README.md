59f60b0201d1fe5672d7d1d066422a4ed30db50c
alexander-daniel <alex@cedarcedar.com>
2021-09-08 12:36:24 -0400

    update effectively wild feed url

---
+++ README.md
@@ -0,0 +1,19 @@
+## a simple next.js podcast webapp
+
+[live example](https://pod.cedarcedar.com)
+
+You can search for podcasts (it uses [gpodder](https://gpodder.net/), a sweet libre web service that has an easy API for searching)
+
+====================
+
+You can paste your own podcast RSS feed to generate a list, e.g
+
+`https://pod.cedarcedar.com/podcast?url=https://blogs.fangraphs.com/feed/effectively-wild/`
+
+where
+
+`https://blogs.fangraphs.com/feed/effectively-wild/`
+
+is the url to the RSS feed for your podcast!
+
+Or you can clone this repo and edit your own list of faves.
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { withRouter } from 'next/router'
-import Layout from '../components/layout';
-import PodcastItem from '../components/podcast-item';
-require('isomorphic-fetch');
+import React from "react";
+import { withRouter } from "next/router";
+import Layout from "../components/layout";
+import PodcastItem from "../components/podcast-item";
+require("isomorphic-fetch");
 
 const FAVES = [
   {
-    title: 'Front Burner',
+    title: "Front Burner",
     description: `Front Burner is your essential daily news podcast, brought to you by CBC News & CBC Podcasts. Every weekday, award-winning investigative journalist Jayme Poisson takes you deep into the stories shaping Canada and the world. Through Jayme’s own reporting and conversations with others in the know, Front Burner will leave you feeling smarter, more connected and eager to share what you've learned.`,
-    url: 'https://www.cbc.ca/podcasting/includes/frontburner.xml'
+    url: "https://www.cbc.ca/podcasting/includes/frontburner.xml",
   },
   {
-    title: 'Effectively Wild',
-    description: 'Baseball statistical analysis and commentary, with Ben, Sam and Meg.',
-    url: 'http://www.baseballprospectus.com/blog/daily_podcast/feed.xml'
+    title: "Effectively Wild",
+    description:
+      "Baseball statistical analysis and commentary, with Ben, Sam and Meg.",
+    url: "https://blogs.fangraphs.com/feed/effectively-wild/",
   },
   {
-    title: 'At the Letters',
-    description: 'Sportsnet Blue Jays weekly podcast with Arden and Ben',
-    url: 'http://feeds.feedburner.com/SN/AtTheLetters'
-  }
+    title: "At the Letters",
+    description: "Sportsnet Blue Jays weekly podcast with Arden and Ben",
+    url: "http://feeds.feedburner.com/SN/AtTheLetters",
+  },
 ];
 
 class FavouritesPage extends React.Component {
-
   static async getInitialProps() {
     return {
-      podcasts: FAVES
-    }
+      podcasts: FAVES,
+    };
   }
 
   render() {
@@ -35,11 +35,11 @@ class FavouritesPage extends React.Component {
 
     return (
       <Layout>
-        {
-          !podcasts.length && router.query.q ?
-          <div>{'No Results'}</div> :
+        {!podcasts.length && router.query.q ? (
+          <div>{"No Results"}</div>
+        ) : (
           podcasts.map((p, i) => <PodcastItem key={i} podcast={p} />)
-        }
+        )}
       </Layout>
     );
   }
